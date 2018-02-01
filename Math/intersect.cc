8ec199f2ac3d714fd40588dec28fe9da685bfb8b
TaiPrev <35993112+TaiPrev@users.noreply.github.com>
2018-02-01 10:49:26 +0100

    Add files via upload

---
+++ Math/intersect.cc
@@ -0,0 +1,112 @@
+#include <cmath>
+#include "intersect.h"
+#include "constants.h"
+#include "tools.h"
+
+/* | algo           | difficulty | */
+/* |----------------+------------| */
+/* | BSPhereBSPhere |          1 | */
+/* | BSPherePlane   |          2 | */
+/* | BBoxBBox       |          3 | */
+/* | BSphereBBox    |          5 | */
+/* | BBoxPlane      |          5 | */
+
+
+// @@ TODO: test if two BBoxes intersect.
+//! Returns :
+//    IINTERSECT intersect
+//    IREJECT don't intersect
+
+int  BBoxBBoxIntersect(const BBox *bba, const BBox *bbb ) {
+
+}
+
+// @@ TODO: test if a BBox and a plane intersect.
+//! Returns :
+//   +IREJECT outside
+//   -IREJECT inside
+//    IINTERSECT intersect
+
+int  BBoxPlaneIntersect (const BBox *theBBox, Plane *thePlane) {
+
+}
+
+
+// @@ TODO: test if two BSpheres intersect.
+//! Returns :
+//    IREJECT don't intersect
+//    IINTERSECT intersect
+
+int BSphereBSphereIntersect(const BSphere *bsa, const BSphere *bsb ) {
+
+}
+
+// @@ TODO: test if a BSpheres intersects a plane.
+//! Returns :
+//   +IREJECT outside
+//   -IREJECT inside
+//    IINTERSECT intersect
+
+int BSpherePlaneIntersect(const BSphere *bs, Plane *pl) {
+
+}
+
+// @@ TODO: test if a BSpheres intersect a BBox.
+//! Returns :
+//    IREJECT don't intersect
+//    IINTERSECT intersect
+
+int BSphereBBoxIntersect(const BSphere *sphere, const BBox *box) {
+
+}
+
+
+int IntersectTriangleRay(const Vector3 & P0,
+						 const Vector3 & P1,
+						 const Vector3 & P2,
+						 const Line *l,
+						 Vector3 & uvw) {
+	Vector3 e1(P1 - P0);
+	Vector3 e2(P2 - P0);
+	Vector3 p(crossVectors(l->m_d, e2));
+	float a = e1.dot(p);
+	if (fabs(a) < Constants::distance_epsilon) return IREJECT;
+	float f = 1.0f / a;
+	// s = l->o - P0
+	Vector3 s(l->m_O - P0);
+	float lu = f * s.dot(p);
+	if (lu < 0.0 || lu > 1.0) return IREJECT;
+	Vector3 q(crossVectors(s, e1));
+	float lv = f * q.dot(l->m_d);
+	if (lv < 0.0 || lv > 1.0) return IREJECT;
+	uvw[0] = lu;
+	uvw[1] = lv;
+	uvw[2] = f * e2.dot(q);
+	return IINTERSECT;
+}
+
+/* IREJECT 1 */
+/* IINTERSECT 0 */
+
+const char *intersect_string(int intersect) {
+
+	static const char *iint = "IINTERSECT";
+	static const char *prej = "IREJECT";
+	static const char *mrej = "-IREJECT";
+	static const char *error = "IERROR";
+
+	const char *result = error;
+
+	switch (intersect) {
+	case IINTERSECT:
+		result = iint;
+		break;
+	case +IREJECT:
+		result = prej;
+		break;
+	case -IREJECT:
+		result = mrej;
+		break;
+	}
+	return result;
+}
